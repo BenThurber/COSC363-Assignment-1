f6cd6470877aa66f141cfd0d72c77e4f8f387aab
Benjamin Thurber <bgt26@uclive.ac.nz>
2020-04-10 22:48:44 +1200

    Added an up and down bobbing motion to the boat.  Fixed the normal vectors on the rudder of the boat.

--- Assignment1/tesla_boat.cpp
+++ Assignment1/tesla_boat.cpp
@@ -18,10 +18,14 @@
 #define SPEED 0.2   // Animation Speed
 #define A 15         // Size of figure of 8 motion
 
+#define BOB_FREQUENCY 4.8
+#define BOB_AMPLITUDE 0.3
+
 float boat_color[4] = {0.4500, 0.3098, 0.0, 1.0};
 
-static double x = A;        // Boat's x coord
-static double theta = -90;   // Boat's angle along the curve
+static double x = 0;        // Boat's x coord
+static double theta = -90;  // Boat's angle along the curve
+static float delta_y = 0;   // The amount the boat 'bobs' up and down
 static int x_direction = 1;
 
 
@@ -38,8 +42,10 @@ void tesla_boat(Model* boat)
     
     double z = x_direction * (x * sqrt(pow(A,2) - pow(x,2))) / A;
     
+    std::cout << delta_y << "\n";
+    
     glPushMatrix();
-        glTranslated(x, height, z);
+        glTranslated(x, height + delta_y, z);
         glRotatef(theta, 0, 1, 0);
         glScalef(size, size, size);
         drawModel(boat);
@@ -53,7 +59,7 @@ void tesla_boat(Model* boat)
 
 
 // Increment and Decrement x between -1 and 1
-void set_next_x() {
+void set_x_theta() {
     
     long double dx = (pow(A,2) - 2*pow(x,2)) / (A*sqrt(pow(A,2) - pow(x,2)));
     double speed = 0.00001 + SPEED / (1.6 * fmaxl(fabsl(dx), 1.0));
@@ -61,6 +67,10 @@ void set_next_x() {
     // The angle of the curve is the angle of the tangent vector
     theta = DEG((-1) * x_direction * atan(dx)) + x_direction*90;
     
+    // Bob the boat up and down a little
+    static int phi = 0;
+    phi = (phi + 1) % 360;
+    delta_y = BOB_AMPLITUDE * cos(RAD(BOB_FREQUENCY * phi));
     
     if ( (x + (x_direction * speed)) > A) {
         x_direction = -1;
@@ -76,7 +86,7 @@ void set_next_x() {
 // Move the boat to its next animation frame.  This should be called in each loop of a glutTimerFunc
 void boat_next_frame()
 {
-    set_next_x();
+    set_x_theta();
 }
 
 
